7746bd540a19be733672dfbf73b420fdf3703a12
Jacob Ross <survivaldawn@gmail.com>
2017-04-28 19:28:12 +0000

    update

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var passport = require("passport");
-var LocalStrategy = require("passport-local");
-var Campground = require("./models/campground");
-var Comment = require("./models/comment");
-var User = require("./models/user");
-var seedDB = require("./seeds");
-
-//----------------------------------------------------------------------------
-
-mongoose.connect("mongodb://localhost/yelp_camp_v3");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
+mongoose.connect("mongodb://localhost/yelp_camp_v10");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-console.log(__dirname);
-seedDB();
+app.use(methodOverride("_method"));
+// seedDB(); //seed the database
 
-// PASSPORT CONFIGURATION -----------------------------------------------------
-
+// PASSPORT CONFIGURATION
 app.use(require("express-session")({
-    secret: "well there it is.", 
+    secret: "Once again Rusty wins cutest dog!",
     resave: false,
     saveUninitialized: false
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -37,146 +39,11 @@ app.use(function(req, res, next){
    next();
 });
 
-// ROUTES ---------------------------------------------------------------------
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.get("/", function(req, res){
-   res.render("landing"); 
-});
-
-//---------------------------------------------------------------------------
-
-app.get("/campgrounds", function(req, res){
-    // get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds){
-       if (err) {
-           console.log(err);
-       } else {
-           res.render("campgrounds/index", {campgrounds:allCampgrounds, currentUser: req.user});
-       }
-    });
-});
-
-//----------------------------------------------------------------------------
-
-app.post("/campgrounds", function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description}
-    // create a new campground and save to DB
-    Campground.create(newCampground, function(err, newlyCreated){
-       if (err) {
-           console.log(err);
-       } else {
-           res.redirect("/campgrounds");
-       }
-    });
-});
-
-//----------------------------------------------------------------------------
-
-app.get("/campgrounds/new", function(req,res){
-   res.render("campgrounds/new"); 
-});
-
-// SHOW ROUTE ----------------------------------------------------------------
-
-app.get("/campgrounds/:id", function(req, res){
-    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-       if (err) {
-           console.log(err);
-       } else {
-           console.log(foundCampground);
-           res.render("campgrounds/show", {campground: foundCampground});
-       }
-    });
-});
-
-// COMMENTS ROUTE -------------------------------------------------------------
-
-app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
-    // find campground by id
-    Campground.findById(req.params.id, function(err, campground){
-       if (err) {
-           console.log(err);
-       } else {
-           res.render("comments/new", {campground: campground});
-       }
-    });
-});
-
-app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
-    // Look up campground using ID.
-    Campground.findById(req.params.id, function(err, campground){
-       if (err) {
-           console.log(err);
-           res.redirect("/campgrounds");
-       } else {
-           Comment.create(req.body.comment, function(err, comment){
-              if (err) {
-                  console.log(err);
-              } else {
-                  campground.comments.push(comment);
-                  campground.save();
-                  res.redirect("/campgrounds/" + campground._id);
-              }
-           });
-       }
-    });
-});
-
-// AUTH ROUTES ----------------------------------------------------------------
-
-// REGISTER ------
-
-app.get("/register", function(req, res){
-   res.render("register"); 
-});
-
-app.post("/register", function(req, res){
-   var newUser = new User({username: req.body.username});
-   User.register(newUser, req.body.password, function(err, user){
-      if (err) {
-          console.log(err);
-          return res.render("register");
-      } 
-      passport.authenticate("local")(req, res, function(){
-         res.redirect("/campgrounds"); 
-      });
-   });
-});
-
-// LOGIN ------
-
-app.get("/login", function(req, res){
-   res.render("login"); 
-});
-
-app.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/campgrounds",
-        failureRedirect: "/login"
-    }), function(req, res){
-});
-
-// LOGOUT ------
-
-app.get("/logout", function(req, res) {
-   req.logout();
-   res.redirect("/campgrounds");
-});
-
-// MIDDLEWARE CHECKING LOGGED IN STATUS ------
-
-function isLoggedIn(req, res, next){
-    if (req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
-
-//----------------------------------------------------------------------------
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("the road to hell is paved with good servers"); 
+   console.log("The YelpCamp Server Has Started!");
 });
--- seeds.js
+++ seeds.js
@@ -1,64 +1,58 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
-var Comment = require("./models/comment");
-var data = [
-        {
-            name: "Davy Crockett National Forest", 
-            image: "https://ourlifewithaview.files.wordpress.com/2012/09/davy-crockett-national-forest-48.jpg",
-            description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-            
-        },
-        {
-            name: "Angelina National Forest", 
-            image: "http://www.texasmonthly.com/wp-content/uploads/2014/05/tripguides_interior4.jpg",
-            description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-            
-        },
-        {
-            name: "Sam Houston National Forest", 
-            image: "http://asergeev.com/pictures/archives/2012/1046/jpeg/06.jpg",
-            description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-            
-        },
-        {
-            name: "Sabine National Forest", 
-            image: "https://jefflynchdev.files.wordpress.com/2011/04/lakeshore_large.jpg",
-            description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
-            
-        }
-    ];
+var Comment   = require("./models/comment");
 
-// Remove all data.
-function seedDB() {
-       Campground.remove({}, function(err){
-       if (err) {
-           console.log(err);
-       } else {
-           console.log("data scrubbed.");
-       }
-       // Add a campground.
-    data.forEach(function(seed){
-        Campground.create(seed, function(err, campground){
-            if (err) {
-                console.log(err);
-            } else {
-                console.log("added a new campground.");
-                Comment.create({
-                   text: "Great place but lots of wild hogs.",
-                   author: "Wilbur the Belly Buster"
-                }, function(err, comment){
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        campground.comments.push(comment);
-                        campground.save();
-                        console.log("created new comment.");
-                    }
-                });
-            }
+var data = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    }
+]
+
+function seedDB(){
+   //Remove all campgrounds
+   Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+         //add a few campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log("added a campground");
+                    //create a comment
+                    Comment.create(
+                        {
+                            text: "This place is great, but I wish there was internet",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);
+                                campground.save();
+                                console.log("Created new comment");
+                            }
+                        });
+                }
+            });
         });
-    });
-  }); 
+    }); 
+    //add a few comments
 }
 
 module.exports = seedDB;
